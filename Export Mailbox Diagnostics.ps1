41103760ee79052ae4da7b5b612fa1e3d03f1b07
LordJeff45 <55820772+LordJeff45@users.noreply.github.com>
2026-04-18 15:30:05 -0600

    Add files via upload

---
+++ Export Mailbox Diagnostics.ps1
@@ -0,0 +1,9 @@
+﻿Connect-ExchangeOnline
+
+$mailbox = "user@domain"
+
+Export-MailboxDiagnosticLogs -ComponentName MRM -Identity $mailbox
+
+Export-MailboxDiagnosticLogs -ComponentName MRM -Identity $mailbox | Export-Csv -Path "$HOME\nameofthefile.csv"
+
+#From <https://learn.microsoft.com/en-us/powershell/module/exchange/export-mailboxdiagnosticlogs?view=exchange-ps> 
