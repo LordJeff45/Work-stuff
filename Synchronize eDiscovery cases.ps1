4f1b99ff7ddc28cf0b59fe7b2371ab7c63f769fd
LordJeff45 <55820772+LordJeff45@users.noreply.github.com>
2026-04-18 15:20:07 -0600

    Add files via upload

---
+++ Synchronize eDiscovery cases.ps1
@@ -0,0 +1,9 @@
+﻿Connect-IPPSSession -EnableSearchOnlySession
+#keep in mind to use connect-ippssession -EnableSearchOnlySession for this
+$AllCases = Get-ComplianceCase
+$AllCases.count
+$AllSearches = $AllCases | ForEach-Object { Get-ComplianceSearch -ResultSize Unlimited -Case $_.Name | ForEach-Object { Write-Host "Search: $($_.Name)"; $_ } }
+$AllSearches.count
+$AllSearches += Get-ComplianceSearch -ResultSize Unlimited | ForEach-Object { Write-Host "ContentSearch: $($_.Name)"; $_ }
+$AllSearches.count
+foreach ($srch in $AllSearches) { set-compliancesearch -identity $srch.name}
