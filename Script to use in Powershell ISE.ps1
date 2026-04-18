6425bb33393bf48b8fc8ffe50884faf0b696baa0
LordJeff45 <55820772+LordJeff45@users.noreply.github.com>
2026-04-18 14:57:12 -0600

    Add files via upload

---
+++ Script to use in Powershell ISE.ps1
@@ -0,0 +1,8 @@
+﻿$msalPath = [System.IO.Path]::GetDirectoryName((Get-Module ExchangeOnlineManagement).Path);
+Add-Type -Path "$msalPath\Microsoft.IdentityModel.Abstractions.dll";
+Add-Type -Path "$msalPath\Microsoft.Identity.Client.dll";
+[Microsoft.Identity.Client.IPublicClientApplication] $application = [Microsoft.Identity.Client.PublicClientApplicationBuilder]::Create("fb78d390-0c51-40cd-8e17-fdbfab77341b").WithDefaultRedirectUri().Build();
+$result = $application.AcquireTokenInteractive([string[]]"https://outlook.office365.com/.default").ExecuteAsync().Result;
+Connect-ExchangeOnline -AccessToken $result.AccessToken -UserPrincipalName $result.Account.Username;
+Connect-IPPSSession  -AccessToken $result.AccessToken -UserPrincipalName $result.Account.Username -EnableSearchOnlySession
+
