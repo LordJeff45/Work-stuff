34af8d3fe87faa28b62b3886e6354a97946662a8
LordJeff45 <55820772+LordJeff45@users.noreply.github.com>
2026-04-18 14:45:30 -0600

    Add files via upload

---
+++ Set junk mailbox config.ps1
@@ -0,0 +1,14 @@
+﻿Connect-ExchangeOnline ;; Connect-IPPSSession
+
+$AllMailboxes = Get-Mailbox -RecipientTypeDetails UserMailbox -ResultSize Unlimited
+# Add the trusted sender domain to each mailbox
+foreach ($Mailbox in $AllMailboxes) {
+   Set-MailboxJunkEmailConfiguration -Identity $Mailbox.Identity -TrustedSendersAndDomains @{Add="senderaddress@domain"}
+}
+```
+```
+$AllMailboxes = Get-Mailbox -RecipientTypeDetails UserMailbox -ResultSize Unlimited
+# Add the trusted sender domain to each mailbox
+foreach ($Mailbox in $AllMailboxes) {
+   Set-MailboxJunkEmailConfiguration -Identity $Mailbox.Identity -TrustedSendersAndDomains @{Remove="senderaddress@domain"}
+}
