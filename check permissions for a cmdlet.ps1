24cd713a63eb46a9e1fa2982f733d8d390cdd6a4
LordJeff45 <55820772+LordJeff45@users.noreply.github.com>
2026-04-18 14:38:59 -0600

    Add files via upload

---
+++ check permissions for a cmdlet.ps1
@@ -0,0 +1,9 @@
+﻿#from <https://learn.microsoft.com/en-us/powershell/exchange/find-exchange-cmdlet-permissions?view=exchange-ps> 
+
+# $cmdlet_to_check = "Write cmdlet before running" #<Cmdlet> [-CmdletParameters <Parameter1>,<Parameter2>,...]
+
+Connect-ExchangeOnline
+$Perms = Get-ManagementRole -Cmdlet $cmdlet_to_check
+$Perms | foreach {Get-ManagementRoleAssignment -Role $_.Name -Delegating $false | Format-Table -Auto Role,RoleAssigneeType,RoleAssigneeName}
+
+
