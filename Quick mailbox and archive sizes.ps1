5221d15845062586e043ee49bb61a2921ba46060
LordJeff45 <55820772+LordJeff45@users.noreply.github.com>
2026-04-18 15:00:56 -0600

    Add files via upload

---
+++ Quick mailbox and archive sizes.ps1
@@ -0,0 +1,14 @@
+﻿Connect-ExchangeOnline
+
+$u = "affected account you want to use for the option"
+$primary = Get-MailboxStatistics -Identity $u
+$archive = Get-MailboxStatistics -Identity $u -Archive
+[PSCustomObject]@{
+   User                     = $u
+   PrimaryMailboxSize       = $primary.TotalItemSize
+   PrimaryItemCount         = $primary.ItemCount
+   PrimaryDeletedItemSize   = $primary.TotalDeletedItemSize
+   ArchiveMailboxSize       = $archive.TotalItemSize
+   ArchiveItemCount         = $archive.ItemCount
+   ArchiveDeletedItemSize   = $archive.TotalDeletedItemSize
+}
